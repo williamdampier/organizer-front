e408df630912eb7399a3fb4d005542a34dcbca1a
williamdampier <74879032+williamdampier@users.noreply.github.com>
2021-12-06 14:28:16 +1300

    header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
+import Organizer from "./components/Organizer";
 import './App.css';
 
 
@@ -8,8 +9,8 @@ import './App.css';
 function App() {
   return (
     <div>
-   
     <Header/>
+    <Organizer/>
     </div>
     
   );
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 const Item = props => {
     return (
         <div classname="item-card">
-            Item Sample
+            <h3><span><img className="icon" src={require('../images/favicon.ico').default} alt="icon" /></span>Item Title</h3>
+            <img src={require('../images/stationery.png').default} alt='description'/>           
+            <p>Description</p>
+            <a href="www.google.com">Item Link</a>
+            
         </div>
     );
 };
