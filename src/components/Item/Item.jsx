1d250ee8f70478f182db22a08c9ea40febc48413
williamdampier <74879032+williamdampier@users.noreply.github.com>
2022-01-28 13:07:04 +1300

    delete button

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import DeleteButton from '../UI/DeleteButton/DeleteButton';
 import styles from "./Item.module.css"
 
 
 const Item = (props) => {
+
+    
+
     return (
         <div className={styles.item}>
-            <h3>{props.title}</h3>
+            <div className={styles.header}>
+                <h3 style={{"alignSelf": "center"}}>{props.title}</h3>
+                <DeleteButton 
+                    option="item"
+                    item_id={props.id}
+                />
+            </div>
+            
             <img className={styles.icon} src={require('../../images/stationery.png').default} alt='description'/>           
             <p>{props.description}</p>
             <a href={'//' + props.link} target="_blank" rel="noopener noreferrer">Go to website</a>
--- src/components/UI/DeleteButton/DeleteButton.jsx
+++ src/components/UI/DeleteButton/DeleteButton.jsx
@@ -1,10 +1,25 @@
+import axios from 'axios';
 import React from 'react';
+import styles from "./DeleteButton.module.css"
+import icon from "./delete.png"
 
+const BASE_URL = process.env.REACT_APP_BASE_URL + "/";
 
-const DeleteButton = ({item_id}) => {
+const DeleteButton = ({option, item_id}) => {
+
+  // window.location.reload();
+
+    function deleteItem() {
+        const path = BASE_URL + option + "/" + item_id
+        axios.delete(path)
+            .then((response) => console.log(response))
+            .catch((err) => console.log(err))
+            window.location.reload();
+    }
+
     return (
-        <div>
-            <input type="submit"/>
+        <div className={styles.button} onClick={deleteItem}>
+            <img className={styles.icon} src={icon}/>
         </div>
     );
 };
